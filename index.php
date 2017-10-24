40367763bfe29fbfbbee1bce06a1d23eb9331213
161710121 <161710121@smkassalaambandung.sch.id>
2017-10-24 17:14:55 +0400

    staff

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+class staff
+{
+	public $nama, $nip, $staff;
+
+	public function __construct ($nama, $nip, $staff)
+	{
+		$this->nama = $nama;
+		$this->nip = $nip;
+		$this->staff = $staff;
+	}
+	
+}
+?>
---
+++ tampil.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'index.php';
+
+$a = new staff('Adit', 161710121, 'Tata Usaha');
+echo "Nama Staff : ".$a->nama."<br>";
+echo "Nip Staff : ".$a->nip."<br>";
+echo "Bagian Staff : ".$a->staff."<br>";
+?>
